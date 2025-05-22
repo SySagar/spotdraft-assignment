b71fc74c6015f1fa170b9fee2bedabd9a27ba1f6
sysagar <sysagar07@gmail.com>
2025-05-22 21:30:43 +0530

    feat: bootstrap vite+shadcnUI client

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <p
+        className='text-2xl'
+      >Test Hello</p>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
